707231ad7b401da4453bb2310d039228dafa87b1
Vladimir Fedzkin <Vovanstik@gmail.com>
2024-12-26 00:20:17 +0300

    sumArrays complete

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -37,8 +37,14 @@ function getIntervalArray(start, end) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  const maxLength = Math.max(arr1.length, arr2.length);
+
+  return Array.from({ length: maxLength }, (_, i) => {
+    const val1 = arr1[i] !== undefined ? arr1[i] : 0;
+    const val2 = arr2[i] !== undefined ? arr2[i] : 0;
+    return val1 + val2;
+  });
 }
 
 /**
