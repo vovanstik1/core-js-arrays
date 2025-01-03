4bc0ae70872e2e6b81da6640cf24327eaf985893
Vladimir Fedzkin <Vovanstik@gmail.com>
2025-01-04 01:03:48 +0300

    feat: removeFalsyValues complite

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -94,8 +94,9 @@ function findAllOccurrences(arr, item) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  const filteredArr = arr.filter(Boolean);
+  return filteredArr;
 }
 
 /**
