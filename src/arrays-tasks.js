bf3abc98449318f23ce6280ad4a3e44f6fff52b5
Vladimir Fedzkin <Vovanstik@gmail.com>
2025-01-04 15:18:53 +0300

    feat: getAverage complite

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -127,8 +127,12 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  const sum = arr.reduce((total, value) => total + value);
+  return Math.round((sum / arr.length) * 100) / 100;
 }
 
 /**
