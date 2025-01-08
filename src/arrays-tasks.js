8f750de1607fc0739989f2af99108f724e4b3508
Vladimir <vovanstik@gmail.com>
2025-01-08 12:58:47 +0300

    feat:findCommonElements complite

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -476,8 +476,8 @@ function getMaxItems(/* arr, n */) {
  *    findCommonElements(['a', 'b', 'c'], ['b', 'c', 'd']) => [ 'b', 'c' ]
  *    findCommonElements([1, 2, 3], ['a', 'b', 'c']) => []
  */
-function findCommonElements(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function findCommonElements(arr1, arr2) {
+  return arr1.filter((value) => arr2.includes(value));
 }
 
 /**
