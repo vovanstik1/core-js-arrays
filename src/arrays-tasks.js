02cd3cca6584c5e64d703053dc910be711b71be7
Vladimir <vovanstik@gmail.com>
2025-01-08 12:24:13 +0300

    feat:generateOdds complite

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -359,8 +359,8 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array.from({ length: len }, (_, i) => 2 * i + 1);
 }
 
 /**
