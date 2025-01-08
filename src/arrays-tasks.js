2d8f5d75cc19c3de47a26cc0fcb36dc52b999462
Vladimir <vovanstik@gmail.com>
2025-01-08 12:54:29 +0300

    feat:getFalsyValuesCount complite

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -391,8 +391,8 @@ function getElementByIndices(arr, indices) {
  *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
  *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
  */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.reduce((count, value) => count + !value, 0);
 }
 
 /**
